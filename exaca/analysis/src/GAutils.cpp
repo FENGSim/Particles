e59b5be932b9246b3a04cdbe69fa5be7155c02ef
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-05-26 09:40:57 +0800

    c

---
+++ exaca/analysis/src/GAutils.cpp
@@ -0,0 +1,91 @@
+// Copyright Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
+// See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#include "GAutils.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+// Parse log file using json format
+void parseLogFile(std::string log_file, int &nx, int &ny, int &nz, double &deltax, int &number_of_layers,
+                  std::vector<double> &xyz_bounds, std::string &grain_unit_vector_file,
+                  bool orientation_files_in_input) {
+
+    std::ifstream input_data_stream;
+    input_data_stream.open(log_file);
+    nlohmann::json logdata = nlohmann::json::parse(input_data_stream);
+
+    // X, Y, Z bounds of domain (in microns)
+    xyz_bounds[0] = logdata["Domain"]["XBounds"][0];
+    xyz_bounds[1] = logdata["Domain"]["YBounds"][0];
+    xyz_bounds[2] = logdata["Domain"]["ZBounds"][0];
+    xyz_bounds[3] = logdata["Domain"]["XBounds"][1];
+    xyz_bounds[4] = logdata["Domain"]["YBounds"][1];
+    xyz_bounds[5] = logdata["Domain"]["ZBounds"][1];
+    // Cell size (in microns)
+    deltax = logdata["Domain"]["CellSize"];
+    // Number of cells per domain direction
+    nx = logdata["Domain"]["Nx"];
+    ny = logdata["Domain"]["Ny"];
+    nz = logdata["Domain"]["Nz"];
+    std::string simulation_type = logdata["SimulationType"];
+    // Directional, Spot, SingleGrain problem types are single layer
+    if (simulation_type == "FromFinch" || simulation_type == "FromFile")
+        number_of_layers = logdata["Domain"]["NumberOfLayers"];
+    else
+        number_of_layers = 1;
+
+    if (orientation_files_in_input)
+        std::cout << "Note: orientation filename specified in log file will be used, overriding value from analysis "
+                     "input file"
+                  << std::endl;
+    grain_unit_vector_file = logdata["GrainOrientationFile"];
+    input_data_stream.close();
+}
+
+// Reads and ignored ASCII field data - each line contains data for a specific z coordinate
+void readIgnoreASCIIField(std::ifstream &input_data_stream, int nz) {
+    std::string line;
+    for (int i = 0; i < nz; i++)
+        getline(input_data_stream, line);
+}
+
+// Get the scaling factor to convert a number of cells into a length, area, or volume
+double convertToMicrons(double deltax, std::string region_type) {
+    double cell_to_size_scaling;
+    if (region_type == "volume")
+        cell_to_size_scaling = Kokkos::pow(deltax, 3) * Kokkos::pow(10, 18);
+    else if (region_type == "area")
+        cell_to_size_scaling = Kokkos::pow(deltax, 2) * Kokkos::pow(10, 12);
+    else if (region_type == "length")
+        cell_to_size_scaling = deltax * Kokkos::pow(10, 6);
+    else
+        throw std::runtime_error("Error: unknown region type");
+    return cell_to_size_scaling;
+}
+
+// Get the scaling factor to convert a length, area, or volume into a number of cells
+double convertToCells(double deltax, std::string region_type) {
+    double size_to_cell_scaling;
+    if (region_type == "volume")
+        size_to_cell_scaling = 1.0 / (pow(deltax, 3) * Kokkos::pow(10, 18));
+    else if (region_type == "area")
+        size_to_cell_scaling = 1.0 / (pow(deltax, 2) * Kokkos::pow(10, 12));
+    else if (region_type == "length")
+        size_to_cell_scaling = 1.0 / (deltax * Kokkos::pow(10, 6));
+    else
+        throw std::runtime_error("Error: unknown region type");
+    return size_to_cell_scaling;
+}
+
+// Print the string "output" to both output file streams
+void dualPrint(std::string output, std::ostream &stream1, std::ostream &stream2) {
+    stream1 << output;
+    stream2 << output;
+}
---
+++ exaca/src/CAinterfacialresponse.hpp
@@ -0,0 +1,85 @@
+// Copyright Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
+// See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef EXACA_IRF_HPP
+#define EXACA_IRF_HPP
+
+#include "CAinputdata.hpp"
+
+#include <Kokkos_Core.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Interfacial response function with various functional forms.
+struct InterfacialResponseFunction {
+
+    InterfacialResponseInputs _inputs;
+
+    // Constructor
+    InterfacialResponseFunction(const double deltat, const double deltax, const InterfacialResponseInputs inputs)
+        : _inputs(inputs) {
+        normalize(deltat, deltax);
+    }
+
+    void normalize(const double deltat, const double deltax) {
+        if (_inputs.function == _inputs.cubic) {
+            // Normalize all 4 coefficients: V = A*x^3 + B*x^2 + C*x + D
+            _inputs.A *= static_cast<float>(deltat / deltax);
+            _inputs.B *= static_cast<float>(deltat / deltax);
+            _inputs.C *= static_cast<float>(deltat / deltax);
+            _inputs.D *= static_cast<float>(deltat / deltax);
+        }
+        else if (_inputs.function == _inputs.quadratic) {
+            // Normalize the 3 relevant coefficients: V = A*x^2 + B*x + C
+            _inputs.A *= static_cast<float>(deltat / deltax);
+            _inputs.B *= static_cast<float>(deltat / deltax);
+            _inputs.C *= static_cast<float>(deltat / deltax);
+        }
+        else if (_inputs.function == _inputs.power) {
+            // Normalize only the leading and last coefficient: V = A*x^B + C
+            _inputs.A *= static_cast<float>(deltat / deltax);
+            _inputs.C *= static_cast<float>(deltat / deltax);
+        }
+    }
+
+    // Compute velocity from local undercooling.
+    // functional form is assumed to be cubic if not explicitly given in input file
+    KOKKOS_INLINE_FUNCTION
+    float compute(const float loc_u) const {
+        float V;
+        if (_inputs.function == _inputs.quadratic)
+            V = _inputs.A * Kokkos::pow(loc_u, 2.0) + _inputs.B * loc_u + _inputs.C;
+        else if (_inputs.function == _inputs.power)
+            V = _inputs.A * Kokkos::pow(loc_u, _inputs.B) + _inputs.C;
+        else
+            V = _inputs.A * Kokkos::pow(loc_u, 3.0) + _inputs.B * Kokkos::pow(loc_u, 2.0) + _inputs.C * loc_u +
+                _inputs.D;
+        return Kokkos::fmax(0.0, V);
+    }
+
+    std::string functionName() {
+        // Not storing string due to Cuda warnings when constructing on device.
+        if (_inputs.function == _inputs.cubic)
+            return "cubic";
+        else if (_inputs.function == _inputs.quadratic)
+            return "quadratic";
+        else if (_inputs.function == _inputs.power)
+            return "power";
+
+        // Should never make it here
+        return "none";
+    }
+
+    auto A() { return _inputs.A; }
+    auto B() { return _inputs.B; }
+    auto C() { return _inputs.C; }
+    auto D() { return _inputs.D; }
+    auto freezingRange() { return _inputs.freezing_range; }
+};
+
+#endif
